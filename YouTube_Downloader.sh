b11ce3151327d88a68a09bbf4e617a73b02fb20e
SwadeDotExe <swadecirata@gmail.com>
2023-02-26 11:06:51 -0500

    Working as intended

--- YouTube_Downloader.sh
+++ YouTube_Downloader.sh
@@ -28,7 +28,7 @@ yes '' | sed 2q
 
 # Send Discord Notification
 SECONDS=0
-PAYLOAD=" { \"content\": \"YouTube-DL started encoding the list of links.\" }"
+PAYLOAD=" { \"content\": \"YouTube-DL started downloading the list of links.\" }"
 curl -X POST -H 'Content-Type: application/json' -d "$PAYLOAD" "$WEBHOOK_URL"
 
 trap "exit" INT
@@ -36,63 +36,86 @@ trap "exit" INT
 # Start loop to read through the links file.
 while read CURRENT; do
 
-# Finds the channel name and link to it.
-NAME=$(echo "$CURRENT" | cut -d, -f1)
-LINK=$(echo "$CURRENT" | cut -d, -f2)
-DOWNLOADTYPE=$(echo "$CURRENT" | cut -d, -f3)
+    # Finds the channel name and link to it.
+    NAME=$(echo "$CURRENT" | cut -d, -f1)
+    LINK=$(echo "$CURRENT" | cut -d, -f2)
+    DOWNLOADTYPE=$(echo "$CURRENT" | cut -d, -f3)
 
-# Echo out variables
-echo " ----------------------------------"
-echo "|         Current Channel          |"
-echo " ----------------------------------"
-echo "Channel:       $NAME"
-echo "Channel Link:  $LINK"
-echo "Download Type: $DOWNLOADTYPE"
+    # Echo out variables
+    echo " ----------------------------------"
+    echo "|         Current Channel          |"
+    echo " ----------------------------------"
+    echo "Channel:       $NAME"
+    echo "Channel Link:  $LINK"
+    echo "Download Type: $DOWNLOADTYPE"
 
-# Download Type: 1=Video 2=Audio+Video
-# NOTE: All downloads are currently audio+video, mainly 
-#       because I don't use video only right now. 
+    # Download Type: 1=Video 2=Audio+Video
+    # NOTE: All downloads are currently audio+video, mainly 
+    #       because I don't use video only right now. 
 
-# Creates new folders for the channel.
-echo "Making directories..."
-sudo mkdir -p $DOWNLOAD_DIR
-sudo mkdir -p $DOWNLOAD_DIR/"$NAME"
-sudo mkdir -p $DOWNLOAD_DIR/"$NAME"/Thumbnails
+    # Creates new folders for the channel.
+    sudo mkdir -p $DOWNLOAD_DIR
+    sudo mkdir -p $DOWNLOAD_DIR/"$NAME"
+    sudo mkdir -p $DOWNLOAD_DIR/"$NAME"/Thumbnails
 
-# Tells status.txt that the download has started
-# TODO: Implement a better system to do this locking
-sudo truncate -s 0 $DOWNLOAD_DIR/"$NAME"/status.txt
-sudo echo "downloading" > $DOWNLOAD_DIR/"$NAME"/status.txt
+    # # Tells status.txt that the download has started
+    # # TODO: Implement a better system to do this locking
+    sudo truncate -s 0 $DOWNLOAD_DIR/"$NAME"/status.txt
+    sudo echo "downloading" > $DOWNLOAD_DIR/"$NAME"/status.txt
 
-# Downloads all the new videos and stores the video-id in a file.
-sudo yt-dlp -ciw -o $DOWNLOAD_DIR/"$NAME"/"%(playlist_autonumber)s_%(title)s.%(ext)s" $LINK --playlist-reverse --add-metadata -f bestvideo+bestaudio/best --write-thumbnail --embed-thumbnail --merge-output-format mkv --embed-subs --write-auto-sub --download-archive $DOWNLOAD_DIR/"$NAME"/titles.txt --cookies $SCRIPT_DIR/YouTubeCookie.txt
+    # Get count of titles file (if exists)
+    if [ -f "$DOWNLOAD_DIR/$NAME/titles.txt" ]; then
+        TXTNUM=$(wc -l $DOWNLOAD_DIR/"$NAME"/titles.txt | awk '{ print $1 }')
+    else
+        TXTNUM=0
+    fi
+    
+    # Get count of channel videos
+    CHANNELNUM=`yt-dlp $LINK -J --flat-playlist | jq ".entries | length"`
+    
+    # Check to see if any new videos since last update
+    if [ "$CHANNELNUM" -gt "$TXTNUM" ]; then
+        echo "New videos detected, running download script."
+    
 
-# Converts all thumbnails to .JPG
-echo "Converting thumbnails to JPG..."
-cd $DOWNLOAD_DIR/"$NAME"/
-mogrify -format jpg -define webp:lossless=true *.webp
-mogrify -format jpg -define *.png
+        #Downloads all the new videos and stores the video-id in a file.
+        sudo yt-dlp -ciw -o $DOWNLOAD_DIR/"$NAME"/"%(playlist_autonumber)s_%(title)s.%(ext)s" $LINK --playlist-reverse --add-metadata -f bestvideo+bestaudio/best --write-thumbnail --embed-thumbnail --merge-output-format mkv --embed-subs --write-auto-sub --download-archive $DOWNLOAD_DIR/"$NAME"/titles.txt --cookies $SCRIPT_DIR/YouTubeCookie.txt --max-downloads 10
 
-# Moves all downloaded thumbnails to a folder.
-echo "Moving thumbnails to folder..."
-sudo find $DOWNLOAD_DIR/"$NAME"/ -name '*jpg' -maxdepth 1 -exec mv -t $DOWNLOAD_DIR/"$NAME"/Thumbnails {} +
+        ################################################
+        #   NOTE: Right now yt-dlp is limited to       #
+        #        10 downloads per run for testing      #
+        ################################################
 
-# Deletes all remaining junk files.
-echo "Deleting unconverted thumbnails..."
-cd $DOWNLOAD_DIR/"$NAME"/
-sudo find . \( -name "*.webp" -o -name "*.png" \) -type f -delete
+        # Converts all thumbnails to .JPG
+        echo "Converting thumbnails to JPG..."
+        cd $DOWNLOAD_DIR/"$NAME"/
+        mogrify -format jpg -define webp:lossless=true *.webp
+        mogrify -format jpg -define *.png
 
-# Move channel cover to thumbnails
-echo "Moving cover to thumbnails folder..."
-cd $DOWNLOAD_DIR/"$NAME"/Thumbnails
-sudo mv NA_*.jpg channel_cover.jpg
+        # Moves all downloaded thumbnails to a folder.
+        echo "Moving thumbnails to folder..."
+        sudo find $DOWNLOAD_DIR/"$NAME"/ -name '*jpg' -maxdepth 1 -exec mv -t $DOWNLOAD_DIR/"$NAME"/Thumbnails {} +
 
-# Tells status.txt that the download is done and can now be encoded.
-sudo truncate -s 0 $DOWNLOAD_DIR/"$NAME"/status.txt
-sudo echo "needsencoding" > $DOWNLOAD_DIR/"$NAME"/status.txt
+        # Deletes all remaining junk files.
+        echo "Deleting unconverted thumbnails..."
+        cd $DOWNLOAD_DIR/"$NAME"/
+        sudo find . \( -name "*.webp" -o -name "*.png" \) -type f -delete
+
+        # Move channel cover to thumbnails
+        echo "Moving cover to thumbnails folder..."
+        cd $DOWNLOAD_DIR/"$NAME"/Thumbnails
+        sudo mv NA_*.jpg channel_cover.jpg
+
+    else
+        echo "No new videos detected, going to next channel."
+    fi
+
+    # Tells status.txt that the download is done and can now be encoded.
+    sudo truncate -s 0 $DOWNLOAD_DIR/"$NAME"/status.txt
+    sudo echo "needsencoding" > $DOWNLOAD_DIR/"$NAME"/status.txt
 
 # Ends the loop.
-done <$SCRIPT_DIR/List_of_Links.txt
+done <List_of_Links.txt
 
 #End timer function
 if (( $SECONDS > 3600 )); then
--- YouTube_Encode.sh
+++ YouTube_Encode.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Starting YouTube_Encode.sh"
+
+# Print blank lines
+yes '' | sed 2q
+
 # Read in Variables
 VARS=`cat credentials.txt`
 WEBHOOK_URL=$(echo "$VARS" | cut -d, -f1)
@@ -8,11 +13,25 @@ VIDEO_DIR=$(echo "$VARS" | cut -d, -f3)
 PODCAST_DIR=$(echo "$VARS" | cut -d, -f4)
 SCRIPT_DIR=$(echo "$VARS" | cut -d, -f5)
 
+# Echo Variables
+echo " ----------------------------------"
+echo "|           Variables:             |"
+echo " ----------------------------------"
+echo "Discord Webhook:    $WEBHOOK_URL"
+echo "Download Directory: $DOWNLOAD_DIR"
+echo "Video Directory:    $VIDEO_DIR"
+echo "Podcast Directory:  $PODCAST_DIR"
+echo "Script Directory:   $SCRIPT_DIR"
+
+# Print blank lines
+yes '' | sed 2q
+
 # Send Discord Notification
 SECONDS=0
 PAYLOAD=" { \"content\": \"YouTube-DL started encoding the list of links.\" }"
-
 curl -X POST -H 'Content-Type: application/json' -d "$PAYLOAD" "$WEBHOOK_URL"
+
+trap "exit" INT
 
 # Start loop to read through the links file.
 while read CURRENT; do
@@ -21,12 +40,24 @@ while read CURRENT; do
 NAME=$(echo "$CURRENT" | cut -d, -f1)
 LINK=$(echo "$CURRENT" | cut -d, -f2)
 DOWNLOADTYPE=$(echo "$CURRENT" | cut -d, -f3)
+
 # Download Type: 1=Video 2=Audio+Video
+# NOTE: All downloads are currently audio+video, mainly 
+#       because I don't use video only right now. 
 
-
+# Get status from file
 STATUS=$(<$DOWNLOAD_DIR/"$NAME"/status.txt)
+
+# Verifies that the download has completed
 if [ $STATUS = "needsencoding" ]; then
-	echo "Starting the process..."
+
+	# Echo out variables
+	echo " ----------------------------------"
+	echo "|         Current Channel          |"
+	echo " ----------------------------------"
+	echo "Channel:       $NAME"
+	echo "Channel Link:  $LINK"
+	echo "Download Type: $DOWNLOADTYPE"
 
 	# Creates new folders for the channel.
 	echo "Making directories..."
@@ -34,62 +65,64 @@ if [ $STATUS = "needsencoding" ]; then
 	sudo mkdir -p $VIDEO_DIR/"$NAME"
 	sudo mkdir -p $VIDEO_DIR/"$NAME"/Thumbnails/
 
-# Makes a file that will tell the upload script to skip this channel because it is still encoding.
-echo "Changing status file to 'encoding'..."
-sudo touch $DOWNLOAD_DIR/"$NAME"/status.txt
-sudo echo "encoding" > $DOWNLOAD_DIR/"$NAME"/status.txt
+	# Updated status to encoding
+	sudo touch $DOWNLOAD_DIR/"$NAME"/status.txt
+	sudo echo "encoding" > $DOWNLOAD_DIR/"$NAME"/status.txt
 
-# Converts to mkv
-cd "$DOWNLOAD_DIR/$NAME"/
+	cd "$DOWNLOAD_DIR/$NAME"/
 
-echo "Starting ffmpeg loop..."
-for i in *.mkv *.webm *.avi *.mov;
-  do video=`echo "$i" | cut -d'.' -f1`
-  echo "
+	# Transcode video loop
+	for i in *.mkv *.webm *.avi *.mov;
+	do video=`echo "$i" | cut -d'.' -f1`
 
-	Transcoding Video
---------------------------
-	Current video: $i
+		yes '' | sed 2q
+		echo "Current video: $i"
+		yes '' | sed 2q
 
+		# Transcode video
+		ffmpeg -y -nostdin -hide_banner -i "$i" -acodec aac -vcodec copy -c:s mov_text "${video}.mp4"
+			
+		# Add thumbnail to video file
+		sudo AtomicParsley "${video}.mp4" --artwork "$DOWNLOAD_DIR/$NAME/Thumbnails/${video}"*.jpg --overWrite
 
-	"
-  ffmpeg -y -nostdin -hide_banner -i "$i" -acodec aac -vcodec copy -c:s mov_text "${video}.mp4"
-	sudo AtomicParsley "${video}.mp4" --artwork "$DOWNLOAD_DIR/$NAME/Thumbnails/${video}.jpg" --overWrite
-	touch -r "$i" "${video}.mp4"
-	echo "Removing old video"
-	sudo rm "$DOWNLOAD_DIR/$NAME/$i"
-done
+		# Fix date to original
+		touch -r "$i" "${video}.mp4"
 
-# Extract audio (if specified)
-if [ $DOWNLOADTYPE = "2" ]; then
-	cd "$DOWNLOAD_DIR/$NAME"
-	for i in *.mp4;
-  	do video=`echo "$i" | cut -d'.' -f1`
-		echo "
-
-		Extracting Audio
---------------------------
-Current video: $i
-
-
-	"
-	#Rips the audio
-	ffmpeg -n -nostdin -hide_banner -i "$i" -vn -c:a aac "$PODCAST_DIR/$NAME/${video}.m4a"
-	#Sets the thumbnail (if applicable)
-	sudo AtomicParsley "$PODCAST_DIR/$NAME/${video}.m4a" --artwork "$DOWNLOAD_DIR/$NAME/Thumbnails/channel_cover.jpg" --overWrite --album "$NAME" --genre "Podcasts" --tracknum `echo "$i" | cut -d'_' -f1` --disk 1
-	#Sets the modified time to the original time
-	touch -r "$i" "$PODCAST_DIR/$NAME/${video}.m4a"
+		# Delete original
+		sudo rm "$DOWNLOAD_DIR/$NAME/$i"
 	done
-fi
 
-# Moves the videos to the long-term storage directory
-sudo mv "$DOWNLOAD_DIR/$NAME"/*.mp4 $VIDEO_DIR/"$NAME"/
-sudo mv "$DOWNLOAD_DIR/$NAME"/Thumbnails/* $VIDEO_DIR/"$NAME"/Thumbnails/
+	# Transcode audio loop
+	if [ $DOWNLOADTYPE = "2" ]; then
 
-# Tells status.txt that the download is done and can now be uploaded.
-echo "Setting status file to done"
-sudo truncate -s 0 $DOWNLOAD_DIR/"$NAME"/status.txt
-sudo echo "done" > $DOWNLOAD_DIR/"$NAME"/status.txt
+		cd "$DOWNLOAD_DIR/$NAME"
+
+		for i in *.mp4;
+		do video=`echo "$i" | cut -d'.' -f1`
+
+			yes '' | sed 2q
+			echo "Current audio: $i"
+			yes '' | sed 2q
+
+			# Extract audio
+			ffmpeg -n -nostdin -hide_banner -i "$i" -vn -c:a aac "$PODCAST_DIR/$NAME/${video}.m4a"
+
+			# Add metadata to audio file
+			sudo AtomicParsley "$PODCAST_DIR/$NAME/${video}.m4a" --artwork "$DOWNLOAD_DIR/$NAME/Thumbnails/channel_cover.jpg" --overWrite --album "$NAME" --genre "Podcasts" --tracknum `echo "$i" | cut -d'_' -f1` --disk 1
+
+			# Fix date to original
+			touch -r "$i" "$PODCAST_DIR/$NAME/${video}.m4a"
+		done
+	fi
+
+	# Moves the videos to the long-term storage directory
+	sudo mv "$DOWNLOAD_DIR/$NAME"/*.mp4 $VIDEO_DIR/"$NAME"/
+	sudo mv "$DOWNLOAD_DIR/$NAME"/Thumbnails/* $VIDEO_DIR/"$NAME"/Thumbnails/
+
+	# Tells status.txt that the download is done and can now be uploaded.
+	echo "Setting status file to done"
+	sudo truncate -s 0 $DOWNLOAD_DIR/"$NAME"/status.txt
+	sudo echo "done" > $DOWNLOAD_DIR/"$NAME"/status.txt
 
 # Ends the loop.
 else
@@ -97,7 +130,7 @@ else
 fi
 
 # Ends the loop.
-done <$SCRIPT_DIR/List_of_Links.txt
+done <List_of_Links.txt
 
 #End timer function
 if (( $SECONDS > 3600 )); then
