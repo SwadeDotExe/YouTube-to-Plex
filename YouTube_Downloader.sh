a60bd8d1ba62df5bdb12c4bdfc28f3916dd79d64
SwadeDotExe <swadecirata@gmail.com>
2023-02-26 08:11:51 -0500

    Tested and verified downloader script

--- YouTube_Downloader.sh
+++ YouTube_Downloader.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Starting YouTube_Downloader.sh"
+
+# Print blank lines
+yes '' | sed 2q
+
 # Read in Variables
 VARS=`cat credentials.txt`
 WEBHOOK_URL=$(echo "$VARS" | cut -d, -f1)
@@ -7,6 +12,19 @@ DOWNLOAD_DIR=$(echo "$VARS" | cut -d, -f2)
 VIDEO_DIR=$(echo "$VARS" | cut -d, -f3)
 PODCAST_DIR=$(echo "$VARS" | cut -d, -f4)
 SCRIPT_DIR=$(echo "$VARS" | cut -d, -f5)
+
+# Echo Variables
+echo " ----------------------------------"
+echo "|           Variables:             |"
+echo " ----------------------------------"
+echo "Discord Webhook:    $WEBHOOK_URL"
+echo "Download Directory: $DOWNLOAD_DIR"
+echo "Video Directory:    $VIDEO_DIR"
+echo "Podcast Directory:  $PODCAST_DIR"
+echo "Script Directory:   $SCRIPT_DIR"
+
+# Print blank lines
+yes '' | sed 2q
 
 # Send Discord Notification
 SECONDS=0
@@ -23,18 +41,26 @@ NAME=$(echo "$CURRENT" | cut -d, -f1)
 LINK=$(echo "$CURRENT" | cut -d, -f2)
 DOWNLOADTYPE=$(echo "$CURRENT" | cut -d, -f3)
 
+# Echo out variables
+echo " ----------------------------------"
+echo "|         Current Channel          |"
+echo " ----------------------------------"
+echo "Channel:       $NAME"
+echo "Channel Link:  $LINK"
+echo "Download Type: $DOWNLOADTYPE"
+
 # Download Type: 1=Video 2=Audio+Video
 # NOTE: All downloads are currently audio+video, mainly 
 #       because I don't use video only right now. 
 
 # Creates new folders for the channel.
+echo "Making directories..."
 sudo mkdir -p $DOWNLOAD_DIR
 sudo mkdir -p $DOWNLOAD_DIR/"$NAME"
 sudo mkdir -p $DOWNLOAD_DIR/"$NAME"/Thumbnails
 
 # Tells status.txt that the download has started
 # TODO: Implement a better system to do this locking
-echo "Setting status file to downloading"
 sudo truncate -s 0 $DOWNLOAD_DIR/"$NAME"/status.txt
 sudo echo "downloading" > $DOWNLOAD_DIR/"$NAME"/status.txt
 
@@ -42,21 +68,26 @@ sudo echo "downloading" > $DOWNLOAD_DIR/"$NAME"/status.txt
 sudo yt-dlp -ciw -o $DOWNLOAD_DIR/"$NAME"/"%(playlist_autonumber)s_%(title)s.%(ext)s" $LINK --playlist-reverse --add-metadata -f bestvideo+bestaudio/best --write-thumbnail --embed-thumbnail --merge-output-format mkv --embed-subs --write-auto-sub --download-archive $DOWNLOAD_DIR/"$NAME"/titles.txt --cookies $SCRIPT_DIR/YouTubeCookie.txt
 
 # Converts all thumbnails to .JPG
+echo "Converting thumbnails to JPG..."
 cd $DOWNLOAD_DIR/"$NAME"/
 mogrify -format jpg -define webp:lossless=true *.webp
 mogrify -format jpg -define *.png
 
 # Moves all downloaded thumbnails to a folder.
+echo "Moving thumbnails to folder..."
 sudo find $DOWNLOAD_DIR/"$NAME"/ -name '*jpg' -maxdepth 1 -exec mv -t $DOWNLOAD_DIR/"$NAME"/Thumbnails {} +
 
 # Deletes all remaining junk files.
+echo "Deleting unconverted thumbnails..."
+cd $DOWNLOAD_DIR/"$NAME"/
 sudo find . \( -name "*.webp" -o -name "*.png" \) -type f -delete
 
+# Move channel cover to thumbnails
+echo "Moving cover to thumbnails folder..."
 cd $DOWNLOAD_DIR/"$NAME"/Thumbnails
 sudo mv NA_*.jpg channel_cover.jpg
 
 # Tells status.txt that the download is done and can now be encoded.
-echo "Setting status file to done"
 sudo truncate -s 0 $DOWNLOAD_DIR/"$NAME"/status.txt
 sudo echo "needsencoding" > $DOWNLOAD_DIR/"$NAME"/status.txt
 
